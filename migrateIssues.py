4ec8d3da40a8a0f3b718b1c0d78593b261cb955f
Phillip Roberts <p@philliproberts.dev>
2021-07-06 15:02:36 -0400

    migrate issues complete

--- migrateIssues.py
+++ migrateIssues.py
@@ -1,64 +1,44 @@
 import os, sys, requests, json, time, random
 import settings 
-from . import common
+import common
 
-sourceuser = sys.argv[1]
-sourcetoken = sys.argv[2]
-targetuser = sys.argv[3]
-targettoken = sys.argv[4]
-orgname = sys.argv[5]
-orgname2 = sys.argv[6]
-repofile = sys.argv[7]
+
 rate = ""
 params = {}
 
-def get_rate_reset():
-    r = requests.get("{settings.base_url}/rate_limit", headers=settings.headers)
-    rate = json.loads(r.text)
-    if rate["resources"]["core"]["remaining"] >= 2:
-        print(f'{rate["resources"]["core"]["remaining"]} API calls until I have to take a break.')
-        return rate 
-    else:
-        reset_time = rate["resources"]["core"]["reset"] - time.time()
-        print(f'Waiting for rate API rate limit to reset... Sleeping for {reset_time} seconds.')
-        time.sleep(reset_time)
-        
 
 # Get All Issues from source
-reponame = common.get_repos(repofile)
-for repo in reponame: 
-    query_url = f"{settings.base_url}/repos/{orgname}/{repo}/issues"
+def get_issues(org, repo):
+    query_url = f"https://{settings.source_url}/repos/{org}/{repo}/issues"
     params = {}
-    r = requests.get(query_url, headers=settings.headers, params=params)
+    r = requests.get(query_url, headers=settings.source_headers, params=params)
     issues = json.loads(r.text)
-    common.get_rate_reset()
+    return issues 
 
 
-for issue in issues:
-    try:
-        json_object = issue
-        print (f"JSON is valid, creating issue: ", issue["id"])
-        query_url = f"{settings.base_url}/repos/{orgname2}/{repo}/issues"
-        payload = {
-            "title": issue["title"],
-            "body": issue["body"],
-        }
-        with open("payload.json", "w") as write_payload:
-            json.dump(payload, write_payload, indent=2, sort_keys=False)
-            write_payload.close()
-        p = requests.request("POST", query_url, data=json.dumps(payload), headers=settings.headers)
-        common.get_rate_reset()
-        if p.status_code == 201:
-            print('Successfully created Issue ID: ', issue["id"])
-            time.sleep(random.randrage(1, 5))
-        elif p.status_code == 403:
-            print(f"Github told us to slow down, so I am taking a breath!") 
-            time.sleep(random.randrange(10, 60))
-        else:
-            print(f"Could not create Issue ID: ", issue["id"])
-            #print(f"Response: {p.status_code}\nMessage: {p.content}")
-            print(p.status_code)
-            print(p.headers)
-    except ValueError as e:
-        print ("Is valid json? false")
+def create_issue(org, repo, issue):
+    query_url = f"https://{settings.target_url}/repos/{org}/{repo}/issues"
+    payload = {
+        "title": issue["title"],
+        "body": issue["body"],
+    }
+    p = requests.request("POST", query_url, data=json.dumps(payload), headers=settings.target_headers)
+    return p 
+
+def migrate_issues(org, repo, issues):
+    for issue in issues:
+        try:
+            json_object = issue
+            print (f"Creating issue: {issue['id']} in {org}/{repo}.")
+            p = create_issue(org, repo, issue)
+            if p.status_code == 201:
+                print(f"Successfully created Issue ID: {issue['id']} in {org}/{repo}.")
+                time.sleep(random.randrange(1, 5))
+            elif p.status_code == 403:
+                print(f"Github told us to slow down, so I am taking a breath!") 
+                time.sleep(random.randrange(10, 60))
+            else:
+                print(f"Could not create Issue ID: {issue['id']} in {org}/{repo} - Gave status code {p.status_code}")
+        except ValueError as e:
+            print ("Is valid json? false")
 
