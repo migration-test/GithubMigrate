176db397bc9ca668a2b6a09cba306b3c728903be
Phillip Roberts <phillip.roberts@bakerhughes.com>
2021-07-09 10:29:12 -0500

    Fixed for newline endings because windows is stupid

--- migrateRepo.py
+++ migrateRepo.py
@@ -11,11 +11,11 @@ def cloneSource(org, repo, user, pat):
     subprocess.run(f'git clone {cloneurl}.git --mirror', shell=True)
 
 def rewriteRefs(repo): 
-    file = open(f"{repo}.git/packed-refs", "rt")
+    file = open(f"{repo}.git/packed-refs", "rt", newline="\n")
     data = file.read()
-    data = data.replace('pull', 'pr')
+    data = data.replace('refs/pull', 'refs/pr')
     file.close()
-    file = open(f"{repo}.git/packed-refs", "wt")
+    file = open(f"{repo}.git/packed-refs", "wt", newline="\n")
     file.write(data)
     file.close()
 
@@ -25,4 +25,4 @@ def pushTarget(org, repo, user, pat):
     targetrepo = f"{settings.target_repo_url}"
     cloneurl = f"https://{targetuser}@{targetrepo}/{org}/{repo}"
     common.create_repo(org, repo)
-    subprocess.run(f'git push {cloneurl} --mirror', cwd=f'./{repo}.git', shell=True)
+    subprocess.run(f'git push {cloneurl} --mirror', cwd=f'.\{repo}.git', shell=True)
