5fcf3bb35b522fb14d93664f69117e9a0906cc18
Phillip Roberts <p@philliproberts.dev>
2021-07-14 13:04:38 -0400

    Extra characters on url....

--- migrateRepo.py
+++ migrateRepo.py
@@ -30,7 +30,7 @@ def pushTarget(org, repo, user, pat):
 
 def delete_repo(org, repo):
     query_url = f"https://{settings.target_api_url}/repos/{org}/{repo}"
-    d = requests.request("DELETE", query_url, headers=settings.target_headers, verify=False)
+    d = requests.request("DELETE", query_url.rstrip(), headers=settings.target_headers, verify=False)
     print(d.request.url)
     print(d.request.body)
     return d
